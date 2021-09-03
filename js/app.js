88b9b4ab80918de290717908b597ff9f30b0106e
rasel <russelroy15@gmail.com>
2021-09-03 16:43:31 +0600

    api call and code apply

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log(99);
+const api_key = `00648c3df9cb0d2748fb5dbdaa57cfae`;
+
+const searchTemperature = () => {
+  const city = document.getElementById("city-name").value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => displayTemperature(data.main));
+};
+
+const displayTemperature = (temperature) => {
+  console.log(temperature.temp);
+};
