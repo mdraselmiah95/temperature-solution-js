0d99a32fd10bb9e576801c1ccf5c6ee6d2bc796f
rasel <russelroy15@gmail.com>
2021-09-03 17:19:05 +0600

    final touch

--- js/app.js
+++ js/app.js
@@ -2,13 +2,25 @@ const api_key = `00648c3df9cb0d2748fb5dbdaa57cfae`;
 
 const searchTemperature = () => {
   const city = document.getElementById("city-name").value;
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=metric`;
 
   fetch(url)
     .then((res) => res.json())
-    .then((data) => displayTemperature(data.main));
+    .then((data) => displayTemperature(data));
+};
+
+const setInnerText = (id, text) => {
+  document.getElementById(id).innerText = text;
 };
 
 const displayTemperature = (temperature) => {
-  console.log(temperature.temp);
+  //   console.log(temperature);
+  setInnerText("city", temperature.name);
+  setInnerText("temperature", temperature.main.temp);
+  setInnerText("condition", temperature.weather[0].main);
+
+  //set weather icon
+  const iconUrl = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
+  const imgIcon = document.getElementById("weather-icon");
+  imgIcon.setAttribute("src", iconUrl);
 };
